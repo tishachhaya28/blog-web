8bba52398addbb42eaddb9836764599a3d66b212
tisha <tishachhaya2@gmail.com>
2026-04-21 20:29:44 +0530

    Fix unused Blog7 import in User.tsx

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -1,7 +1,6 @@
 import { UserList } from './UserList'
 import { SidebarProvider } from './ui/sidebar'
 import { AppSidebar } from './app-sidebar'
-import { Blog7 } from './blog7'
 
 function User() {
   return (
